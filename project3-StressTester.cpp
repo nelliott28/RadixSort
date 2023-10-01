1d2c603d774aac1d0fcb6ca11c74748c663a0112
ShadeStar44 <119338592+ShadeStar44@users.noreply.github.com>
2023-10-01 12:49:34 -0600

    Final Version of the Stress Tester Code

--- project3-StressTester.cpp
+++ project3-StressTester.cpp
@@ -5,23 +5,48 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+
 using namespace std;
+
 int main()
 {
-	srand(time(0));
+	srand(time(0)); // Allows all the random num generator s to be different by generating differnt time stamps for them
+
+	// Intizlizes the dynamic array and generates a random size for it
 	int n = 0;
-	n = rand() % 10000+1;
+	n = rand() % 1000;
 	cout << n << endl;
 	string* arr = new string[n];
+	bool nOrS = false;
 
-	for (int i = 0; i < n; i++) 
-	{
-		int newN = rand() % 1000;
+	if (nOrS == true) {
+		// Allocates random numbers to each element
+		for (int i = 0; i < n; i++)
+		{
+			int newN = rand() % 10000000;
 
-		string tempN = to_string(newN);
-		arr[n] = tempN;
-
+			string tempN = to_string(newN);
+			arr[i] = tempN;
+			//cout << arr[i] << endl;
+		}
+		delete[] arr;
 	}
-	delete[] arr;
+	else if (nOrS == false) {
+		for (int i = 0; i < n; i++)
+		{
+			// Generats random string of characters for testing
+			int numCh = rand() %3+1;
+			string randLet = " ";
+			for (int j = 0;  j < numCh; j++) {
+				char currentChar = 'a' + rand() % 26;
+				randLet += currentChar;
+				//cout << randLet << endl;
+			}
+
+			arr[i] = randLet;
+			//cout << arr[i] << endl;
+		}
+		delete[] arr;
+	}
 	return 0;
 }
